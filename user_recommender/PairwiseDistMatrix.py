a14d2cf175015bbe4d6bf133bf5d41629c7873ca
beingzy <beingzy@gmail.com>
2016-04-05 22:49:12 -0400

    add target_user_ids in PairwiseDistMatrix to reduce distance calculation

--- user_recommender/PairwiseDistMatrix.py
+++ user_recommender/PairwiseDistMatrix.py
@@ -9,11 +9,15 @@ from distance_metrics import GeneralDistanceWrapper
 
 class PairwiseDistMatrix(object):
 
-    def __init__(self, user_ids, user_profiles):
+    def __init__(self, user_ids, user_profiles, target_user_ids=None):
         """ instance initilization function
-        :param user_ids: <list> a list of unique user ids
-        :param user_profiles: <matrix-like> a numpy.array of user profile
-        :return: self
+
+        Parameters:
+        ===========
+        user_ids: <list> a list of unique user ids
+        user_profiles: <matrix-like> a numpy.array of user profile
+        target_uer_ids: <list> a list of user whose distance in relation to other users
+            will be  calculated only.
         """
         if isinstance(user_profiles, DataFrame):
             user_profiles = user_profiles.as_matrix()
@@ -32,9 +36,19 @@ class PairwiseDistMatrix(object):
         # user-related information
         self._user_ids = user_ids
         self._user_profiles = user_profiles
+        self._target_user_ids = target_user_ids
         # user-pair-wise distance related info.
         self._dist_user_pairs = []
         self._dist_matrix = []
+
+        self._all_pairs = []
+        if target_user_ids is None:
+            self._all_pairs = combinations(user_ids, 2)
+        else:
+            other_user_ids = [uid for uid in user_ids if not uid in target_user_ids]
+            for a_uid in target_user_ids:
+                for b_uid in other_user_ids:
+                    self._all_pairs.append((a_uid, b_uid))
 
     def set_dist_func(self, dist_func):
         """ redefine the distance metric function """
@@ -43,7 +57,7 @@ class PairwiseDistMatrix(object):
     def update_distance_matrix(self):
         """ update the distance matrix """
         # is_update = function() change
-        all_pairs = combinations(self._user_ids, 2)
+        all_pairs = self._all_pairs
         dist_user_pairs = []
         dist_containers = []
         for user_a, user_b in all_pairs:
@@ -70,13 +84,24 @@ class PairwiseDistMatrix(object):
         * (user_ids <list>, distance <list>)
         """
         if not user_id in self._user_ids:
-            raise ValueError("user_id %s is not included in data !".format(user_id))
+            raise ValueError("user_id (:{}) is not included in data!".format(user_id))
+
+        if not self._target_user_ids is None:
+            if not user_id in self._target_user_ids:
+                raise ValueError("user_id (:{})is not in target_user_ids!".format(user_id))
+
         # collect index of user-pair whose first user is target user
         idx_b = [ ii for ii, pp in enumerate(self._dist_user_pairs) if pp[0]==user_id ]
         user_id_b = [ pp[1] for pp in self._dist_user_pairs if pp[0]==user_id ]
-        # collect index of user-pair whose second user is target user
-        idx_a = [ ii for ii, pp in enumerate(self._dist_user_pairs) if pp[1]==user_id ]
-        user_id_a = [ pp[0] for pp in self._dist_user_pairs if pp[1]==user_id ]
+
+        if not self._target_user_ids is None:
+            # collect index of user-pair whose second user is target user
+            idx_a = [ ii for ii, pp in enumerate(self._dist_user_pairs) if pp[1]==user_id ]
+            user_id_a = [ pp[0] for pp in self._dist_user_pairs if pp[1]==user_id ]
+        else:
+            idx_a = []
+            user_id_a = []
+
         # counter part user_ids
         cnu_ids = user_id_a + user_id_b
         idx = idx_a + idx_b
