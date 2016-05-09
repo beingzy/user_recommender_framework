115d078ca0899c0480ea79be2508e7ea8a02db02
beingzy <beingzy@gmail.com>
2016-05-09 15:57:43 -0400

    follow up with previous commit

--- user_recommender/GWDUserRecommender.py
+++ user_recommender/GWDUserRecommender.py
@@ -138,6 +138,7 @@ class GWDUserRecommender(UserRecommenderMixin):
             if self._iter_counter == 0:
                 fit_weights, fit_groups = _consolidate_learned_info(self.gwd_learner,
                                                                     self._buffer_min_size)
+                self._fit_weights, self._fit_groups = fit_weights, fit_groups
             else:
                 fit_weights, fit_groups = self._fit_weights, self._fit_groups
 
