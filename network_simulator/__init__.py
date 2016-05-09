7bbbf3c4fc2452df68f217db5b7dba24991a82f4
beingzy <beingzy@gmail.com>
2016-05-09 14:17:55 -0400

    create GuidedUserClickSimulator and its test file

--- network_simulator/__init__.py
+++ network_simulator/__init__.py
@@ -2,9 +2,11 @@ from .UserRecSysExpSimulator import UserRecSysExpSimulator
 from .SocialNetworkEvaluator import EvaluatorMixin
 from .SocialNetworkEvaluator import SocialNetworkEvaluator
 from .UserClickSimulator import UserClickSimulator
+from .UserClickSimulator import GuidedUserClickSimulatior
 
 
 __all__ = ['UserRecSysExpSimulator',
            'EvaluatiorMixin',
            'SocialNetworkEvaluator',
-           'UserClickSimulator']
+           'UserClickSimulator',
+           'GuidedUserClickSimulatior']
---
+++ network_simulator/test/test_GuidedUserClickSimulator.py
@@ -0,0 +1,23 @@
+""" objective introduction
+
+Author: Yi Zhang <beingzy@gmail.com>
+Date: 2016/05/09
+"""
+import unittest
+from ..UserClickSimulator import GuidedUserClickSimulatior
+
+
+class TestGuidedUserClickSimulator(unittest.TestCase):
+
+    def __init__(self):
+        self._user_connections = [[1, 2], [1, 3], \
+                                  [2, 3], [2, 5], \
+                                  [3, 5], [4, 5], \
+                                  [4, 6]]
+
+    def TestRec(self):
+        user_conns = self._user_connections
+        guided_clicker = GuidedUserClickSimulatior(user_conns)
+        accepted, recommended = guided_clicker(1, [2, 3, 4, 5, 6])
+        self.assertEqual(accepted, [2, 3])
+        self.assertEqual(recommended, [4, 5, 6])
