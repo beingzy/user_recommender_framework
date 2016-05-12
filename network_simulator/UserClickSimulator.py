eacbd4fae31e2e639ebc84c763471c4284d560ee
beingzy <beingzy@gmail.com>
2016-05-12 12:10:48 -0400

    minor refactor on GuidedUserClickSimulator

--- network_simulator/UserClickSimulator.py
+++ network_simulator/UserClickSimulator.py
@@ -19,25 +19,6 @@ def zipf_pdf(k, n, s=1):
     return num / den
 
 
-def convert_pair_dictionary(user_connections):
-    """ translate user pairs into user-wise dictionary
-    """
-    user_conn_dict = {}
-    for ii, (uid_a, uid_b) in enumerate(user_connections):
-
-        if uid_a in user_conn_dict:
-            user_conn_dict[uid_a].append(uid_b)
-        else:
-            user_conn_dict[uid_a] = [uid_b]
-
-        if uid_b in user_conn_dict:
-            user_conn_dict[uid_b].append(uid_a)
-        else:
-            user_conn_dict[uid_b] = [uid_a]
-
-    return user_conn_dict
-
-
 class UserClickSimulatorMixin(object):
 
     def click(self, rec_list):
@@ -64,7 +45,7 @@ class UserClickSimulator(UserClickSimulatorMixin):
         return confirmed
 
 
-class GuidedUserClickSimulatior(UserClickSimulatorMixin):
+class GuidedUserClickSimulator(UserClickSimulatorMixin):
 
     def __init__(self, reference_user_connections):
         """
@@ -73,18 +54,37 @@ class GuidedUserClickSimulatior(UserClickSimulatorMixin):
         reference_user_connections: {matrix-like} (n, 2)
             pair of users to represent true status of social network
         """
-        reference_user_connections = convert_pair_dictionary(reference_user_connections)
+        reference_user_connections = _convert_pair_dictionary(reference_user_connections)
         self._ref_user_connections = reference_user_connections
 
     def click(self, target_user_id, rec_list):
         known_user_conns = self._ref_user_connections[target_user_id]
         accepted = []
-        recommended = []
+        rejected = []
         for ii, uid in enumerate(rec_list):
             if uid in known_user_conns:
                 accepted.append(uid)
             else:
-                recommended.append(uid)
+                rejected.append(uid)
 
-        return accepted, recommended
+        return accepted, rejected
+
+
+def _convert_pair_dictionary(user_connections):
+    """ translate user pairs into user-wise dictionary
+    """
+    user_conn_dict = {}
+    for ii, (uid_a, uid_b) in enumerate(user_connections):
+
+        if uid_a in user_conn_dict:
+            user_conn_dict[uid_a].append(uid_b)
+        else:
+            user_conn_dict[uid_a] = [uid_b]
+
+        if uid_b in user_conn_dict:
+            user_conn_dict[uid_b].append(uid_a)
+        else:
+            user_conn_dict[uid_b] = [uid_a]
+
+    return user_conn_dict
 
